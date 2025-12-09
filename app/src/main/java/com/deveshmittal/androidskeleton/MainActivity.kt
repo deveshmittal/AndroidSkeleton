a145868cf988a8db18add1942b1374bb29374bdb
Devesh Mittal <d.mittal@miu.edu>
2025-12-08 20:37:02 -0600

    Add gradle.properties

--- app/src/main/java/com/deveshmittal/androidskeleton/MainActivity.kt
+++ app/src/main/java/com/deveshmittal/androidskeleton/MainActivity.kt
@@ -4,13 +4,23 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.text.input.rememberTextFieldState
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.OutlinedTextField
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
+import androidx.compose.material3.TextField
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.autofill.ContentType
+import androidx.compose.ui.semantics.contentType
+import androidx.compose.ui.semantics.semantics
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import com.deveshmittal.androidskeleton.ui.theme.AndroidSkeletonTheme
 
 class MainActivity : ComponentActivity() {
@@ -18,12 +28,22 @@ class MainActivity : ComponentActivity() {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContent {
-            AndroidSkeletonTheme {
-                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    Greeting(
-                        name = "Android",
-                        modifier = Modifier.padding(innerPadding)
-                    )
+            MaterialTheme {
+                Scaffold(modifier = Modifier.fillMaxSize()) { paddingInfo ->
+                    val textField = rememberTextFieldState()
+                    Column{
+                        TextField(
+                            state = textField,
+                            modifier = Modifier.semantics{
+                                contentType = ContentType.EmailAddress
+                            }
+                        )
+                        Text(
+                            text = "${textField.text}",
+                            modifier = Modifier.size(24.dp)
+                        )
+                    }
+
                 }
             }
         }
